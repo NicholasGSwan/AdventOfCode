dc90576f50822ca2db81a1a57eedaf971bc8bf06
Nicholas <Nicholas@DESKTOP-8O4PAV5>
2017-12-11 23:38:34 -0600

    Finished checksum part 2
    fixed error with j loop

--- Advent of Code/src/Checksum.java
+++ Advent of Code/src/Checksum.java
@@ -41,24 +41,45 @@ public class Checksum {
 		String[] stringArray = stringToArray.split("\t");
 		int[] intArray = new int[stringArray.length];
 		for(int i = 0; i<stringArray.length; i++) {
+			if(!stringArray[i].equals("")) {
 			intArray[i] = Integer.valueOf(stringArray[i]);
+			}
 		}
 		return intArray;
+	}
+	
+	static int getQuotientIfEvenlyDivisible(int[] arr) {
+		int result = 0;
+		for (int i = 0; i < arr.length - 1; i++) {
+			for(int j = i+1; j < arr.length; j++) {
+				if(arr[i] % arr[j] == 0) {
+				result = arr[i] / arr[j];	
+				}
+				if(arr[j] % arr[i] == 0) {
+					result = arr[j] / arr[i];
+				}
+			}
+		}
+		
+		return result;
 	}
 	
 	
 	static int returnChecksum(Scanner in) {
 		int checksum = 0;
 		List<Integer> differences = new ArrayList<>();
+		List<Integer> quotients = new ArrayList<>();
 		String answer = "";
 		do {
 			int[] array = returnArray(in);
+			int quotient = getQuotientIfEvenlyDivisible(array);
+			quotients.add(quotient);
 			int difference = getRowDifference(array);
 			differences.add(difference);
 			System.out.println("is there another row?");
 			answer = in.nextLine();
-		}while(answer.equals("yes"));
-		for(int num: differences) {
+		}while(answer.equalsIgnoreCase("y"));
+		for(int num: quotients) {
 			checksum+= num;
 		}
 		
@@ -71,8 +92,8 @@ public class Checksum {
 		int[] testArray = {1919,2959,82,507,3219,239,3494,1440,3107,259,3544,683,207,562,276,2963};
 		
 		System.out.println(getRowDifference(testArray));
-		int[] testArray2 = returnArray(in);
-		System.out.println(getRowDifference(testArray2));
+//		int[] testArray2 = returnArray(in);
+//		System.out.println(getRowDifference(testArray2));
 		int checksum = returnChecksum(in);
 		System.out.println(checksum);
 		
